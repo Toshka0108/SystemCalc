b6bb00598b4c255caff41fd6ad36d1d0f66e047d
toshka0108 <toshka0108@gmail.com>
2013-02-17 16:31:36 +0400

    Read from a file
    This thing reads system of equations from a file, then divide it into 3
    lists (matrixes) and so on.
    close #1

--- SystemCalc.py
+++ SystemCalc.py
@@ -1,3 +1,5 @@
+import re
+
 class Matrix:
 	mas = []
 	rows = 0
@@ -10,60 +12,72 @@ class Matrix:
 	#	return self.mas[index[0] * self.columns + index[1]]
 
 class equations:
-	t = 0
+    Amat = []
+    Bmat = []
+    Xmat = []
+    AmatHeight = 0
+    AmatWidth = 0 
 
-import re
-file = open("test.txt")
-o = []
-free = []
-while 1:
-    line = file.readline()
-    if not line:
-        break
-    line = line.strip('\n')
-    line = line.replace(' ','')
-    array = line.split('=')
-    digits = re.compile('[0-9]+')
-    lst = digits.findall(line)
-    charts = re.compile("[a-z]")
-    lst1 = charts.findall(line)
-    znaki = re.compile("[^a-zA-Z0-9_]")
-    lst2 = znaki.findall(line)
-    members = array[0]
-    dig = []
-    for i in range(len(lst)):
-    	dig.append(0)
-    if len(lst) == len(lst2):
-    	for i in range(len(lst)):
-    		if lst2[i] == '-':
-    			dig[i] = -int(lst[i])
-    		else:
-    			dig[i] = int(lst[i])
-    else:
-    	dig[0] = int(lst[0])
-    	for i in range(len(lst2)):
-    		if lst2[i] == '-':
-    			dig[i+1] = -int(lst[i+1])
-    		else:
-    			dig[i+1] = int(lst[i+1])
-    #for i in range(len(dig)-1):
-    #	o.append(dig[i])
-    free.append(dig[len(dig)-1])
-    D = {}
-    for i in range(len(lst1)):
-    	D[lst1[i]] = dig[i]
-    b = D.keys()
-    b.sort()
-    for i in b:
-    	o.append(D[i]) 
-    	#o.append(D[b])
-    #members = members.split('+')
-    #members = [x.split('-') for x in members]
-    #a = line.find('=')
-    print lst, lst1, lst2, dig, D,b
-print o, free
+    def __init__(self):
+        file = open("test.txt")
+        o = []
+        self.AmatHeight = 0
+        self.Amat = []
+        self.Bmat = []
+        self.Xmat = []
+        while 1:
+            line = file.readline()
+            if not line:
+                break
+            line = line.strip('\n')
+            line = line.replace(' ','')
+            array = line.split('=')
+            lline = array[0]
+            rline = array[1]
+            digits = re.compile('[0-9]+')
+            lst = digits.findall(lline)
+            charts = re.compile("[a-z]")
+            lst1 = charts.findall(lline)
+            znaki = re.compile("[^a-zA-Z0-9_]")
+            lst2 = znaki.findall(lline)
+            self.AmatHeight = self.AmatHeight+1
+            self.AmatWidth = len(lst)
+            o = []
+            rlst = digits.findall(rline)
+            rlst2 = znaki.findall(rline)
+            for i in range(len(lst)):
+                o.append(0)
+            if len(lst) == len(lst2):
+                for i in range(len(lst)):
+                    if lst2[i] == '-':
+                        o[i] = -int(lst[i])
+                    else:
+                        o[i] = int(lst[i])
+            else:
+                o[0] = int(lst[0])
+                for i in range(len(lst2)):
+                    if lst2[i] == '-':
+                        o[i+1] = -int(lst[i+1])
+                    else:
+                        o[i+1] = int(lst[i+1])
+            if rlst2 == []:
+                self.Bmat.append(int(rlst[0]))
+            else:
+                self.Bmat.append(-int(rlst[0]))
+            D = {}
+            for i in range(len(lst1)):
+                D[lst1[i]] = o[i]
+            self.Xmat = D.keys()
+            self.Xmat.sort()
+            for i in self.Xmat:
+                self.Amat.append(D[i])
+        print self.Amat , self.Bmat, self.Xmat, self.AmatHeight , self.AmatWidth
+
+
+a = equations()
 
 
 
 
-#print Matrix()[1,2]
+
+#print Matrix()
